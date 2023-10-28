058f3c3e5853ce3eb4c3843cc7d8906b0ac6d17d
Thiago Prado <103077237+ThiagoAPrado@users.noreply.github.com>
2023-10-27 22:18:23 -0300

    titulo adc

--- firearm-deaths-analysis/src/App.tsx
+++ firearm-deaths-analysis/src/App.tsx
@@ -1,10 +1,14 @@
 import ContextTextBlock from "./components/ContextTextBlock"
+import TitleBlock from "./components/TitleBlock"
 import { mainPageTypings } from "./typings/mainPageTypings"
+
+import "./index.css"
 
 function App() {
 
   return (
     <>
+      <TitleBlock />
       <div className="m-20 p-20 bg-slate-200">
         {
           mainPageTypings.map(e => {
---
+++ firearm-deaths-analysis/src/components/TitleBlock.tsx
@@ -0,0 +1,11 @@
+
+const TitleBlock = () => {
+  return (
+    <div className="flex items-center bg-slate-800">
+      <h1>COMPARAÇÃO DE MORTES POR ARMAS DE FOGO ENTRE BRASIL E ESTADOS UNIDOS</h1>
+      <h2>Análise do TDE e a relação do número de mortes por arma de fogo entre Brasil e EUA e suas nuances</h2>
+    </div>
+  )
+}
+
+export default TitleBlock
