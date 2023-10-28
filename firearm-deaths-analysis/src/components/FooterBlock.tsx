ea5dd745514fbc2ebd08d6c3d19809d038aaf730
Thiago Prado <103077237+ThiagoAPrado@users.noreply.github.com>
2023-10-28 16:58:33 -0300

    responsive 50%

--- firearm-deaths-analysis/src/components/FooterBlock.tsx
+++ firearm-deaths-analysis/src/components/FooterBlock.tsx
@@ -1,17 +1,18 @@
 const FooterBlock = () => {
     return (
-        <div className="flex flex-row items-start justify-start text-center gap-[8vh] px-[5vw] py-[2.5vw] bg-slate-600 mt-10">
+        <div className="grid grid-rows-3 text-center gap-[2vw] px-[5vw] py-[2.5vw] bg-slate-600 mt-10 md:grid-cols-3">
             <div className="flex flex-col items-start justify-start text-start">
                 <h1 className="text-[1.4rem] font-bold text-white md:text-[1.3rem]">Repositorios do projeto</h1>
-                <a className="text-[1.2rem] text-white underline md:text-[1.4rem] lg:text-[1rem] visited:text-purple-300" href="https://github.com/ThiagoAPrado/firearm-deaths-analysis-BRxUS">https://github.com/ThiagoAPrado/firearm-deaths-analysis-BRxUS</a>
-                <a className="text-[1.2rem] text-white underline md:text-[1.4rem] lg:text-[1rem] visited:text-purple-300" href="https://github.com/vicEduDev/firearm-deaths-analysis-BRxUS-image-bank">https://github.com/vicEduDev/firearm-deaths-analysis-BRxUS-image-bank</a>
+                <a className="text-[1.2rem] text-white underline md:text-[1.4rem] lg:text-[1rem]" href="https://github.com/ThiagoAPrado/firearm-deaths-analysis-BRxUS">https://github.com/ThiagoAPrado/firearm-deaths-analysis-BRxUS</a>
+                <a className="text-[1.2rem] text-white underline md:text-[1.4rem] lg:text-[1rem]" href="https://github.com/vicEduDev/firearm-deaths-analysis-BRxUS-image-bank">https://github.com/vicEduDev/firearm-deaths-analysis-BRxUS-image-bank</a>
             </div>
             
             <div className="flex flex-col items-start justify-start text-start">
                 <h1 className="text-[1.4rem] font-bold text-white md:text-[1.3rem]">Datasets Originais</h1>
-                <a className="text-[1.2rem] text-white underline md:text-[1.4rem] lg:text-[1rem] visited:text-purple-300" href="https://data.world/nkrishnaswami/gun-deaths-by-county">Gun deaths by County</a>
-                <a className="text-[1.2rem] text-white underline md:text-[1.4rem] lg:text-[1rem] visited:text-purple-300" href="https://www.ipea.gov.br/atlasviolencia/filtros-series/5/bitos-por-armas-de-fogo">IPEA - Óbitos por Armas de Fogo</a>
+                <a className="text-[1.2rem] text-white underline md:text-[1.4rem] lg:text-[1rem]" href="https://data.world/nkrishnaswami/gun-deaths-by-county">Gun deaths by County</a>
+                <a className="text-[1.2rem] text-white underline md:text-[1.4rem] lg:text-[1rem]" href="https://www.ipea.gov.br/atlasviolencia/filtros-series/5/bitos-por-armas-de-fogo">IPEA - Óbitos por Armas de Fogo</a>
             </div>
+
             <div className="flex flex-col items-start justify-start text-start">
                 <h1 className="text-[1.4rem] font-bold text-white md:text-[1.3rem]">Sobre nos</h1>
                 <p className="text-[1.2rem] text-white md:text-[1.4rem] lg:text-[1rem] items-start justify-start text-start">Somos alunos do atual 4º semestre do curso de Ciência da Computação da Universidade Católica de Santos. Desenvolvemos essa página para fins de execução de Trabalho Discente Efetivo e para divulgação mais elaborada de dados para futuros trabalho de outros interessados</p>
--- firearm-deaths-analysis/src/components/interestMenu.tsx
+++ firearm-deaths-analysis/src/components/interestMenu.tsx
@@ -2,20 +2,25 @@
 import { useLocation, useNavigate } from "react-router"
 import { MenuItemInterface } from "../interfaces/interface";
 
-const interestMenu = ({menus} : {menus: MenuItemInterface[]}) => {
+const interestMenu = ({ menus }: { menus: MenuItemInterface[] }) => {
     const navigate = useNavigate();
     const location = useLocation();
 
-	return (
-		<div className="flex gap-[0.3vw]">
+    return (
+        <div className="flex ml-[0.3vw] gap-[0.3vw]">
             {
                 menus.map((e) => {
-                    return <h3 className={`flex justify-center items-center h-[2vw] w-[5vw] pt-[.10vw] text-[1vw] rounded-[0_0_0.6vw_.6vw] shadow-bo cursor-pointer ${(location.pathname == e.path ? 'bg-red-700' : 'bg-slate-800')} text-white`} key={"navigate-btn-" + e.title} onClick={() => {navigate(e.path)}}>{e.title}</h3>
+                    return (
+                        <button className={`flex justify-center items-center h-[2vw] px-[0.9vw] text-[1vw] tracking-wider font-bold rounded-[0_0_0.4vw_.4vw] shadow-bo cursor-pointer ${(location.pathname == e.path ? 'bg-orange-500 hover:bg-orange-500' : 'bg-slate-800 hover:bg-slate-600')} text-white `}
+                            key={"navigate-btn-" + e.title}
+                            onClick={() => { navigate(e.path) }}
+                        >{e.title}</button>
+                    )
                 })
             }
         </div>
 
-	)
+    )
 }
 
 export default interestMenu
