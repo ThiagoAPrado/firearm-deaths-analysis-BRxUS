802042fabe1f71615be7a6e48f15abad6aaac1c6
Thiago Prado <103077237+ThiagoAPrado@users.noreply.github.com>
2023-10-28 21:40:11 -0300

    responsive 100%

--- src/components/interestMenu.tsx
+++ src/components/interestMenu.tsx
@@ -3,24 +3,24 @@ import { useLocation, useNavigate } from "react-router"
 import { MenuItemInterface } from "../interfaces/interface";
 
 const interestMenu = ({ menus }: { menus: MenuItemInterface[] }) => {
-    const navigate = useNavigate();
-    const location = useLocation();
+	const navigate = useNavigate();
+	const location = useLocation();
 
-    return (
-        <div className="flex ml-[0.3vw] gap-[0.3vw]">
-            {
-                menus.map((e) => {
-                    return (
-                        <button className={`flex justify-center items-center px-[0.9vw] text-[1vw] tracking-wider font-bold rounded-[0_0_0.4vw_.4vw] shadow-bo cursor-pointer ${(location.pathname == e.path ? 'bg-orange-500 hover:bg-orange-500' : 'bg-slate-800 hover:bg-slate-600')} text-white `}
-                            key={"navigate-btn-" + e.title}
-                            onClick={() => { navigate(e.path) }}
-                        >{e.title}</button>
-                    )
-                })
-            }
-        </div>
+	return (
+		<div className="flex justify-start ml-[5vw] gap-[0.35em] md:ml-[10vw] lg:ml-[15vw] xl:ml-[20vw]">
+			{
+				menus.map((e) => {
+					return (
+						<button className={`flex justify-center items-center px-[2.5vw] py-[1vh] text-[1rem] tracking-wider font-bold rounded-[0_0_.75rem_.75rem] shadow-bo cursor-pointer ${(location.pathname == e.path ? 'bg-orange-500 hover:bg-orange-500' : 'bg-slate-800 hover:bg-slate-600')} text-white `}
+							key={"navigate-btn-" + e.title}
+							onClick={() => { navigate(e.path) }}
+						>{e.title}</button>
+					)
+				})
+			}
+		</div>
 
-    )
+	)
 }
 
 export default interestMenu
